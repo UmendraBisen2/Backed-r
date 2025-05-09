69bd59871e0dd7ae26accde32b49a4216ace922e
Umendra A Bisen <umendrabisen2005@gmail.com>
2025-05-09 07:36:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,3 +3,28 @@ import app from "./app.js";
 app.listen(process.env.PORT, ()=>{
     console.log(`SERVER HAS STARTED AT PORT ${process.env.PORT}`);
 })
+const mongoose=require('mongoose')
+
+mongoose.connect("mongodb://localhost:27017/LoginSignup")
+.then(()=>{
+    console.log("MongoDB Connected Succesfully!");
+})
+.catch(()=>{
+    console.log("Failed to Connect!")
+})
+
+const LogInSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+
+const collection=new mongoose.model("LogInCollection",LogInSchema)
+
+module.exports=collection
